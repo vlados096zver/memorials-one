d5e6fa5e9c7fb5ba497499b3338107d1d2d24bf7
vlados096zver <81378975+vlados096zver@users.noreply.github.com>
2024-12-28 16:59:15 +0200

    jekl

---
+++ src/js/main.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.header-burger');
+  const list = document.querySelector('.header-list');
+
+  if (burger) {
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      list.classList.toggle('active');
+    });
+  }
+});
+
+let currentInput = null;
+let isKeyboardVisible = false; 
+
+document.querySelectorAll('input, textarea').forEach(input => {
+  input.addEventListener('click', function () {
+    currentInput = this;
+    if (isKeyboardVisible) {
+        openKeyboardForInput();
+    }
+  });
+});
+
+document.querySelectorAll('.virtual-block').forEach(element => {
+  element.addEventListener('click', () => {
+    isKeyboardVisible = true;
+    if (currentInput) {
+      openKeyboardForInput();
+    }
+  });
+});
+
+function openKeyboardForInput() {
+  const keyboard = document.querySelector('.keyboard');
+
+  if (currentInput) {
+    const inputRect = currentInput.getBoundingClientRect();
+    
+    const keyboardHeight = keyboard?.offsetHeight; 
+    const viewportHeight = window.innerHeight; 
+
+    const scrollX = window.scrollX || window.pageXOffset;
+    const scrollY = window.scrollY || window.pageYOffset;
+    const x = inputRect.left + scrollX;
+    const y = inputRect.top + scrollY;
+    const heightInput = currentInput.offsetHeight;
+
+    keyboard.style.left = `${x}px`;
+    keyboard.style.top = `${y + heightInput}px`;
+    keyboard.style.display = 'block'; 
+    isKeyboardVisible = true; 
+  }
+}
+
+document.addEventListener('click', (event) => {
+  const keyboard = document.querySelector('.keyboard');
+  const virtualBlocks = document.querySelectorAll('.virtual-block'); // Получаем все .virtual-block
+  const inputElements = document.querySelectorAll('input, textarea');
+
+  // Проверяем, что клик не был сделан по клавиатуре, ни одному из .virtual-block и ни одному из input/textarea
+  const clickedOutside = 
+    !keyboard.contains(event.target) && 
+    !Array.from(virtualBlocks).some(block => block.contains(event.target)) && 
+    !Array.from(inputElements).includes(event.target);
+
+  if (clickedOutside) {
+    keyboard.style.display = 'none';
+    isKeyboardVisible = false;
+  }
+});
